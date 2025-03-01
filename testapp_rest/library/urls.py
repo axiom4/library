073ba5c7335db110b158bb5c1a321819f0cd2146
Riccardo GIannetto <rgiannetto@gmail.com>
2025-03-01 18:21:20 +0100

    Add trailing_slash=False

--- testapp_rest/library/urls.py
+++ testapp_rest/library/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework import routers
 from . import views
 
-router = routers.DefaultRouter()
+router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'books', views.BookViewSet)
 
 urlpatterns = [
