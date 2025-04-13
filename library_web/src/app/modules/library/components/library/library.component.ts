eb050b8948b370b145ed88b29b88c123112110a0
Riccardo GIannetto <rgiannetto@gmail.com>
2025-04-13 07:32:48 +0200

    Add mat-table and mat-sort components

--- library_web/src/app/modules/library/components/library/library.component.ts
+++ library_web/src/app/modules/library/components/library/library.component.ts
@@ -5,13 +5,15 @@ import {
   PaginatedBookList,
   LibraryBooksListRequestParams,
 } from '../../../core/api/v1';
-import { NgFor } from '@angular/common';
+import { DatePipe, NgFor, NgIf } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { PageEvent, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { Sort, MatSortModule, SortDirection } from '@angular/material/sort';
 
 @Component({
   selector: 'app-library',
-  imports: [NgFor, RouterLink, MatPaginatorModule],
+  imports: [NgIf, MatPaginatorModule, MatTableModule, MatSortModule, DatePipe],
   templateUrl: './library.component.html',
   styleUrl: './library.component.scss',
 })
@@ -23,6 +25,13 @@ export class LibraryComponent implements OnInit {
   pageIndex = 0;
   pageEvent: PageEvent | undefined;
   pageSizeOptions = [5, 10, 25];
+  ordering = 'title';
+  displayedColumns: string[] = [
+    'id',
+    'title',
+    'author_name',
+    'publication_date',
+  ];
 
   constructor(private readonly libraryService: LibraryService) {}
 
@@ -38,10 +47,37 @@ export class LibraryComponent implements OnInit {
     this.getBooks();
   }
 
+  handleSortEvent(e: Sort) {
+    const sortDirection: SortDirection = e.direction;
+    let sortActive = e.active;
+
+    if (sortActive === 'publication_date') {
+      sortActive = 'publication_date';
+    } else if (sortActive === 'author_name') {
+      sortActive = 'author';
+    } else if (sortActive === 'title') {
+      sortActive = 'title';
+    } else {
+      sortActive = 'id';
+    }
+
+    console.log('sortActive', sortActive);
+    console.log('sortDirection', sortDirection);
+
+    if (sortDirection === 'asc') {
+      this.ordering = `-${sortActive}`;
+    } else {
+      this.ordering = `${sortActive}`;
+    }
+
+    this.getBooks();
+  }
+
   getBooks() {
     const params: LibraryBooksListRequestParams = {
       page: this.pageIndex + 1,
       pageSize: this.pageSize,
+      ordering: this.ordering,
     };
 
     this.libraryService.libraryBooksList(params).subscribe({
