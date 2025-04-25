f58b2da0be95ca473f6cbd63490f529b1f2daf1b
Riccardo GIannetto <rgiannetto@gmail.com>
2025-04-25 21:07:55 +0200

    Notifications test

--- library_web/src/app/app.component.ts
+++ library_web/src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LibraryComponent } from './modules/library/components/library/library.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { LibraryNotificationService } from './modules/library/services/library-notification.service';
+import { LibraryNotificationComponent } from './modules/library/components/library-notification/library-notification.component';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [RouterOutlet, LibraryComponent, MatButtonModule, MatIconModule],
+  imports: [
+    RouterOutlet,
+    LibraryComponent,
+    MatButtonModule,
+    MatIconModule,
+    LibraryNotificationComponent,
+  ],
 })
 /**
  * The `AppComponent` serves as the root component of the Library application.
@@ -17,10 +26,36 @@ import { MatIconModule } from '@angular/material/icon';
  *
  * @implements {OnInit}
  */
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'Library';
+  constructor(private libraryNotificationService: LibraryNotificationService) {}
 
-  constructor() {}
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.libraryNotificationService.notify({
+        message: 'Welcome to the Library!',
+        type: 'info',
+        duration: 3000,
+      });
+      this.libraryNotificationService.notify({
+        message: 'New book added to the library!',
+        type: 'success',
+        duration: 5000,
+      });
+      this.libraryNotificationService.notify({
+        message: 'Error loading book data.',
+        type: 'error',
+        duration: 4000,
+      });
+      this.libraryNotificationService.notify({
+        message: 'This is a warning notification.',
+        type: 'warning',
+        duration: 6000,
+      });
+    }, 2000); // Simulate a delay before adding notifications
+  }
 
-  ngOnInit(): void {}
+  ngOnDestroy() {
+    this.destroyStream$.next();
+  }
 }
