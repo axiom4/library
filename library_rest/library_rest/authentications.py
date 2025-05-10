b10ba831128ec36e22e42c1006c3886e4bbdd172
Riccardo GIannetto <rgiannetto@gmail.com>
2025-05-10 07:23:26 +0200

    Fix django authentication module

--- library_rest/library_rest/authentications.py
+++ library_rest/library_rest/authentications.py
@@ -18,7 +18,7 @@ from drf_spectacular.extensions import OpenApiAuthenticationExtension
 
 class KeyCloakAuthenticationSchema(OpenApiAuthenticationExtension):
     # full import path OR class ref
-    target_class = 'IpBlocker.authentication.KeyCloakAuthentication'
+    target_class = 'library_rest.authentications.KeyCloakAuthentication'
     name = 'KeyCloakAuthentication'  # name used in the schema
 
     def get_security_definition(self, auto_schema):
